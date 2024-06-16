e61841ebcae2bfc837e7829f0845108ddf1132d6
QUIET-DEVELOPER <venu.nlr001@gmail.com>
2024-06-16 22:43:08 +0530

    docx: update SumOfIndividualDigits.java file

--- PatternsAndBasicProblems/SumOfIndividualDigits.java
+++ PatternsAndBasicProblems/SumOfIndividualDigits.java
@@ -5,12 +5,16 @@ public class SumOfIndividualDigits {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number do you want to sum of individual digits : ");
         int number = sc.nextInt();
+        int sum = sumOfDigits(number);
+        System.out.println("The Sum of Individual Digits of a Number is : " + sum);
+    }
+
+    public static int sumOfDigits(int number) {
         int sum = 0;
         while(number != 0) {
-            int digit = number % 10;
-            sum += digit;
+            sum += number % 10;
             number /= 10;
         }
-        System.out.println("The Sum of Individual Digits of a Number is : " + sum);
+        return sum;
     }
 }
